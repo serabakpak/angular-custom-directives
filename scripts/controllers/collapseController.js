b0d93912ad477da1b483c16e677daabec4ff8840
Sera Bak <serabak15@gmail.com>
2016-09-19 16:12:34 -0700

    adds collapse 3rd party directive

---
+++ scripts/controllers/collapseController.js
@@ -0,0 +1,20 @@
+angular.module('CardsAgainstAssembly')
+	.controller('collapseController', collapseController);
+
+
+function collapseController() {
+	var vm = this;
+	vm.isCollapsed = true;
+	vm.isCollapsedHorizontal = true;
+}
+
+
+
+
+
+
+
+
+
+
+
